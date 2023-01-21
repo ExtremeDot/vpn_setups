8abd67bfeeb9b4d4870591416e4ff47636cfbb3c
DisaterOne <120102306+ExtremeDot@users.noreply.github.com>
2023-01-22 02:44:12 +0330

    Update dot-v2ray.sh

--- dot-v2ray.sh
+++ dot-v2ray.sh
@@ -291,6 +291,216 @@ systemctl start systemd-resolved.service
 green " ================================================== "
 green "  Wireguard installed successfully!"
 green " ================================================== "
+
+function getGithubLatestReleaseVersion(){
+    wget --no-check-certificate -qO- https://api.github.com/repos/$1/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-
+}
+
+versionWgcf="2.2.15"
+downloadFilenameWgcf="wgcf_${versionWgcf}_linux_amd64"
+configWgcfBinPath="/usr/local/bin"
+configWgcfConfigFolderPath="${HOME}/wireguard"
+configWgcfAccountFilePath="${configWgcfConfigFolderPath}/wgcf-account.toml"
+configWgcfProfileFilePath="${configWgcfConfigFolderPath}/wgcf-profile.conf"
+configWARPPortFilePath="${configWgcfConfigFolderPath}/warp-port"
+configWireGuardConfigFileFolder="/etc/wireguard"
+configWireGuardConfigFilePath="/etc/wireguard/wgcf.conf"
+configWireGuardDNSBackupFilePath="/etc/resolv_warp_bak.conf"
+configWarpPort="40000"
+versionWgcf=$(getGithubLatestReleaseVersion "ViRb3/wgcf")
+downloadFilenameWgcf="wgcf_${versionWgcf}_linux_amd64"
+echo
+green " =================================================="
+green " Install Cloudflare WARP CLI tool Wgcf ${versionWgcf}"
+green " =================================================="
+echo
+mkdir -p ${configWgcfConfigFolderPath}
+mkdir -p ${configWgcfBinPath}
+mkdir -p ${configWireGuardConfigFileFolder}
+cd ${configWgcfConfigFolderPath}
+wget -O ${configWgcfConfigFolderPath}/wgcf --no-check-certificate "https://github.com/ViRb3/wgcf/releases/download/v${versionWgcf}/${downloadFilenameWgcf}"
+	if [[ -f ${configWgcfConfigFolderPath}/wgcf ]]; then
+		green "Cloudflare WARP command line tool Wgcf ${versionWgcf} downloaded successfully!"
+		echo
+    else
+		red "  Wgcf ${versionWgcf} download failed! "
+		exit 255
+    fi
+
+chmod +x ${configWgcfConfigFolderPath}/wgcf
+cp ${configWgcfConfigFolderPath}/wgcf ${configWgcfBinPath}
+	if [[ -f ${configWgcfAccountFilePath} ]]; then
+		echo
+	else
+		yes | ${configWgcfConfigFolderPath}/wgcf register 
+	fi
+echo
+echo
+green " =================================================="
+yellow " If you have not purchased a WARP+ subscription, please press Enter to skip this step, Press enter to continue without WARP+"
+echo
+yellow " If you have purchased WARP+ subscription, you can fill in the license key to enable WARP+"
+    green " How to check: Open the Cloudflare 1.1.1.1 app on your phone, click the top right menu click hamburger menu button on the top-right corner "
+    green " Navigate to: Account > Key, select the key in the Account menu is the license key"
+    echo
+
+    read -p "Please Input license key! Press Enter to skip by default, please enter:" isWARPLicenseKeyInput
+    isWARPLicenseKeyInput=${isWARPLicenseKeyInput:-n}
+
+    if [[ ${isWARPLicenseKeyInput} == [Nn] ]]; then
+        echo
+    else 
+        sed -i "s/license_key =.*/license_key = \"${isWARPLicenseKeyInput}\"/g" ${configWgcfAccountFilePath}
+        WGCF_LICENSE_KEY="${isWARPLicenseKeyInput}" wgcf update
+    fi
+
+    if [[ -f ${configWgcfProfileFilePath} ]]; then
+        echo
+    else
+        yes | ${configWgcfConfigFolderPath}/wgcf generate 
+    fi
+    
+
+    cp ${configWgcfProfileFilePath} ${configWireGuardConfigFilePath}
+
+    enableWireguardIPV6OrIPV4
+
+    echo 
+    green " Start Wireguard temporarily, to test it starts normally, run the command: wg-quick up wgcf"
+    ${sudoCmd} wg-quick up wgcf
+
+    echo 
+    green " Start to verify whether Wireguard is started normally, and detect whether to use Cloudflare's ipv6 access !"
+    echo
+    echo "curl -6 ip.p3terx.com"
+    curl -6 ip.p3terx.com 
+    echo
+    isWireguardIpv6Working=$(curl -6 ip.p3terx.com | grep CLOUDFLARENET )
+    echo
+
+    if [[ -n "$isWireguardIpv6Working" ]]; then	
+        green " Wireguard started fine and has successfully reached the network via IPv6 provided by Cloudflare WARP! "
+    else 
+        green " ================================================== "
+        red " Wireguard detects IPV6 access failure using CLOUDFLARENET through curl -6 ip.p3terx.com"
+        red " Please check if the linux kernel is installed correctly"
+        red " The installation will continue to run, and it may be successful, but IPV6 is not used"
+        red " To check whether WireGuard is started successfully, you can run the command to check the running status: systemctl status wg-quick@wgcf"
+        red " If WireGuard fails to start, run the log command to find out why: journalctl -n 50 -u wg-quick@wgcf"
+        red " If WireGuard fails to start, it is recommended to redo the new system, do not replace other kernels, and install WireGuard directly"
+        green " ================================================== "
+    fi
+
+    echo
+    green " To disable Wireguard, which was started temporarily for testing, run the command: wg-quick down wgcf "
+    ${sudoCmd} wg-quick down wgcf
+    echo
+
+    ${sudoCmd} systemctl daemon-reload
+    
+
+    ${sudoCmd} systemctl enable wg-quick@wgcf
+
+
+    ${sudoCmd} systemctl start wg-quick@wgcf
+
+    # (crontab -l ; echo "12 6 * * 1 systemctl restart wg-quick@wgcf ") | sort - | uniq - | crontab -
+
+    checkWireguardBootStatus
+
+    echo
+    green " ================================================== "
+    green "  Wireguard and the Cloudflare WARP command line tool Wgcf ${versionWgcf} installed successfully!"
+    green "  Account configuration file path for Cloudflare WARP application: ${configWgcfAccountFilePath}"
+    green "  Path to Wireguard configuration file generated by Cloudflare WARP: ${configWireGuardConfigFilePath} "
+    echo
+    green "  Wireguard stop command: systemctl stop wg-quick@wgcf Start command: systemctl start wg-quick@wgcf Restart command: systemctl restart wg-quick@wgcf"
+    green "  Wireguard view log: journalctl -n 50 -u wg-quick@wgcf"
+    green "  Check the running status of Wireguard: systemctl status wg-quick@wgcf"
+    echo
+    green "  Use this script to install v2ray or xray, you can choose whether to unlock Netflix restrictions and avoid popping up Google reCAPTCHA man-machine verification!"
+    echo
+    green "  For v2ray or xray installed by other scripts, please replace the v2ray or xray configuration file by yourself!"
+    green "  You can refer to the tutorial on how to use IPv6 to access Netflix https://ybfl.xyz/111.html or https://toutyrater.github.io/app/netflix.html"
+    green " ================================================== "
+
+    # https://p3terx.com/archives/use-cloudflare-warp-to-add-extra-ipv4-or-ipv6-network-support-to-vps-servers-for-free.html
+    
+    
+    ${sudoCmd} systemctl stop wg-quick@wgcf
+
+    cp /etc/resolv.conf ${configWireGuardDNSBackupFilePath}
+
+    sed -i '/nameserver 2a00\:1098\:2b\:\:1/d' /etc/resolv.conf
+
+    sed -i '/nameserver 8\.8/d' /etc/resolv.conf
+    sed -i '/nameserver 9\.9/d' /etc/resolv.conf
+    sed -i '/nameserver 1\.1\.1\.1/d' /etc/resolv.conf
+
+    echo
+    green " ================================================== "
+    yellow " Please choose whether to add IPv6 networking or IPv4 networking support for the server: "
+    echo
+    green " 1 Added IPv6 network (used to unblock Netflix restrictions and avoid Google reCAPTCHA pop-ups)"
+    green " 2 Add IPv4 network (for adding IPv4 network support to IPv6-only VPS hosts)"
+    echo
+    read -p "Please choose to add IPv6 or IPv4 network support? Press Enter to select 1 by default, please enter[1/2]:" isAddNetworkIPv6Input
+	isAddNetworkIPv6Input=${isAddNetworkIPv6Input:-1}
+
+	if [[ ${isAddNetworkIPv6Input} == [2] ]]; then
+
+        # 为 IPv6 Only 服务器添加 IPv4 网络支持
+
+        sed -i 's/^AllowedIPs = \:\:\/0/# AllowedIPs = \:\:\/0/g' ${configWireGuardConfigFilePath}
+        sed -i 's/# AllowedIPs = 0\.0\.0\.0/AllowedIPs = 0\.0\.0\.0/g' ${configWireGuardConfigFilePath}
+
+        sed -i 's/engage\.cloudflareclient\.com/\[2606\:4700\:d0\:\:a29f\:c001\]/g' ${configWireGuardConfigFilePath}
+        sed -i 's/162\.159\.192\.1/\[2606\:4700\:d0\:\:a29f\:c001\]/g' ${configWireGuardConfigFilePath}
+
+        sed -i 's/^DNS = 1\.1\.1\.1/DNS = 2620:fe\:\:10,2001\:4860\:4860\:\:8888,2606\:4700\:4700\:\:1111/g'  ${configWireGuardConfigFilePath}
+        sed -i 's/^DNS = 8\.8\.8\.8,8\.8\.4\.4,1\.1\.1\.1,9\.9\.9\.10/DNS = 2620:fe\:\:10,2001\:4860\:4860\:\:8888,2606\:4700\:4700\:\:1111/g'  ${configWireGuardConfigFilePath}
+        
+        echo "nameserver 2a00:1098:2b::1" >> /etc/resolv.conf
+        
+        echo
+        green " Wireguard has successfully switched to IPv4 network support for VPS servers"
+
+    else
+
+        # 为 IPv4 Only 服务器添加 IPv6 网络支持
+        sed -i 's/^AllowedIPs = 0\.0\.0\.0/# AllowedIPs = 0\.0\.0\.0/g' ${configWireGuardConfigFilePath}
+        sed -i 's/# AllowedIPs = \:\:\/0/AllowedIPs = \:\:\/0/g' ${configWireGuardConfigFilePath}
+
+        sed -i 's/engage\.cloudflareclient\.com/162\.159\.192\.1/g' ${configWireGuardConfigFilePath}
+        sed -i 's/\[2606\:4700\:d0\:\:a29f\:c001\]/162\.159\.192\.1/g' ${configWireGuardConfigFilePath}
+        
+        sed -i 's/^DNS = 1\.1\.1\.1/DNS = 8\.8\.8\.8,8\.8\.4\.4,1\.1\.1\.1,9\.9\.9\.10/g' ${configWireGuardConfigFilePath}
+        sed -i 's/^DNS = 2620:fe\:\:10,2001\:4860\:4860\:\:8888,2606\:4700\:4700\:\:1111/DNS = 8\.8\.8\.8,1\.1\.1\.1,9\.9\.9\.10/g' ${configWireGuardConfigFilePath}
+
+        echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+        echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+        echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+        #echo "nameserver 9.9.9.9" >> /etc/resolv.conf
+        echo "nameserver 9.9.9.10" >> /etc/resolv.conf
+
+        echo
+        green " Wireguard has successfully switched to IPv6 network support for VPS servers"
+    fi
+    
+    green " ================================================== "
+    echo
+    green " Wireguard configuration information is as follows Configuration file path: ${configWireGuardConfigFilePath} "
+    cat ${configWireGuardConfigFilePath}
+    green " ================================================== "
+    echo
+
+    # -n 不为空
+    if [[ -n $1 ]]; then
+        ${sudoCmd} systemctl start wg-quick@wgcf
+    else
+        preferIPV4
+    fi
+
 ;;
 
 
